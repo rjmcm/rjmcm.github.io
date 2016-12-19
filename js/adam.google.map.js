ca3f7f8c9be396d1a89ce4a874ac324282e550a4
rjmcm <ryanjmcmanus@gmail.com>
2016-12-19 15:18:59 -0500

    index-6work

--- js/adam.google.map.js
+++ js/adam.google.map.js
@@ -6,12 +6,12 @@ function init() {
     // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
     var mapOptions = {
         // How zoomed in you want the map to start at (always required)
-        zoom: 11,
+        zoom: 15,
 
         scrollwheel: false,
 
         // The latitude and longitude to center the map (always required)
-        center: new google.maps.LatLng(40.6700, -73.9400), // New York
+        center: new google.maps.LatLng(33.7767, -84.3911), // Tech Square
 
         // How you would like to style the map. 
         // This is where you would paste any style found on Snazzy Maps.
@@ -27,8 +27,8 @@ function init() {
 
     // Let's also add a marker while we're at it
     var marker = new google.maps.Marker({
-        position: new google.maps.LatLng(40.6700, -73.9400),
+        position: new google.maps.LatLng(33.7767, -84.3911),
         map: map,
-        title: 'ADAM!'
+        title: 'Tech Square'
     });
 }
